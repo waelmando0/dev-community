79206ca76693d0d29f86f8d5ed80caf67488f583
waelmando0 <59392618+waelmando0@users.noreply.github.com>
2024-01-25 19:57:29 +0200

    update sidebar

--- components/shared/left-sidebar.tsx
+++ components/shared/left-sidebar.tsx
@@ -6,6 +6,8 @@ import {
 	User2Icon,
 	Briefcase,
 	MessageCircleQuestion,
+	UserCircle,
+	UserPlus,
 } from 'lucide-react';
 
 import React from 'react';
@@ -57,8 +59,8 @@ const LeftSidebar = () => {
 	];
 
 	return (
-		<section className='flex flex-col h-screen sticky top-0 left-0 justify-between overflow-y-auto border-r p-6 pt-36 shadow-sm shadow-zinc-300 dark:shadow-none max-sm:hidden lg:w-[266px] dark:bg-zinc-900 custom-scroll'>
-			<div className='flex flex-1 flex-col gap-6'>
+		<section className='flex flex-col h-screen sticky top-0 left-0 justify-between overflow-y-auto border-r p-6 pt-36 shadow-sm shadow-zinc-300 dark:shadow-none max-sm:hidden lg:w-[266px] bg-white dark:bg-zinc-900 custom-scroll'>
+			<div className='flex flex-1 flex-col gap-6 items-center lg:items-start'>
 				{sidebarLinks.map((item) => (
 					<div key={item.route}>
 						<Link
@@ -71,7 +73,7 @@ const LeftSidebar = () => {
 							)}
 						>
 							<span>{item.icon}</span>
-							<span>{item.label}</span>
+							<span className='max-lg:hidden'>{item.label}</span>
 						</Link>
 					</div>
 				))}
@@ -79,14 +81,16 @@ const LeftSidebar = () => {
 
 			<div className='flex flex-col gap-3'>
 				<Link href='/sign-in'>
-					<Button className='w-full'>
-						<span>Log In</span>
-					</Button>
+					<button className='w-full p-2 inline-flex items-center justify-center h-10 px-4 rounded-xl bg-secondary hover:bg-secondary/90 dark:bg-accent dark:text-white text-sm'>
+						<span className='max-lg:hidden'>Log In</span>
+						<UserCircle className='lg:hidden' />
+					</button>
 				</Link>
 				<Link href='/sign-up'>
-					<Button className='w-full bg-zinc-700'>
-						<span>Sign Up</span>
-					</Button>
+					<button className='w-full p-2 inline-flex items-center justify-center h-10 px-4 rounded-xl bg-primary text-primary-foreground hover:bg-primary/90 dark:bg-zinc-700 dark:text-white text-sm'>
+						<span className='max-lg:hidden'>Sign Up</span>
+						<UserPlus className='lg:hidden' />
+					</button>
 				</Link>
 			</div>
 		</section>
