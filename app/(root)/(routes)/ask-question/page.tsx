a4b3d281212ccb66b2f6ea437701fee48ca2d700
waelmando0 <59392618+waelmando0@users.noreply.github.com>
2024-02-04 17:46:44 +0200

    update

--- app/(root)/(routes)/ask-question/page.tsx
+++ app/(root)/(routes)/ask-question/page.tsx
@@ -5,10 +5,11 @@ import { redirect } from "next/navigation";
 import React from "react";
 
 const Page = async () => {
-	// const { userId } = auth();
-	const userId = "12345";
+	const { userId } = auth();
 
-	if (!userId) redirect("/sign-in");
+	if (!userId) {
+		redirect("/sign-in");
+	}
 
 	const mongoUser = await getUserById({ userId });
 	console.log(mongoUser);
@@ -18,7 +19,7 @@ const Page = async () => {
 			<h1 className="text-3xl font-bold">Ask a question</h1>
 
 			<div className="mt-11">
-				<Question mongoUserId={JSON.stringify(mongoUser._id)} />
+				<Question mongoUserId={JSON.stringify(mongoUser._id)} type="new" />
 			</div>
 		</div>
 	);
